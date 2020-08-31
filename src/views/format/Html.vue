551751eb3c61e891992e49f0b9a88de1efc77538
hhp1614 <hhp1614@qq.com>
2020-08-31 17:57:29 +0800

    feat(format/html): 增加HTML模块

---
+++ src/views/format/Html.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mdui-row">
+    <editor mode="htmlmixed" :value="htmlSource" @input="inputHtml" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Html',
+  data() {
+    return {
+      htmlSource: '<div></div>'
+    }
+  },
+  methods: {
+    inputHtml(val) {
+      this.htmlSource = val
+    }
+  }
+}
+</script>
