55cd8f5799e5da22ea34cb96961aa3e15eec53de
hhp1614 <hhp1614@qq.com>
2020-09-01 16:37:54 +0800

    style(format/css): 格式化代码

--- src/views/format/Css.vue
+++ src/views/format/Css.vue
@@ -3,9 +3,9 @@
     <editor class="line-20" mode="htmlmixed" :value="code" @input="acUpdateCode" />
     <div class="mdui-m-t-1">
       <mdui-btn @click="format" dense ripple>格式化</mdui-btn>
-      <mdui-btn mdui-tooltip="{content:'删除不必要的空格和换行'}" @click="compress(0)" dense ripple>净化</mdui-btn>
+      <mdui-btn mdui-tooltip="{content: '删除不必要的空格和换行'}" @click="compress(0)" dense ripple>净化</mdui-btn>
       <mdui-btn
-        mdui-tooltip="{content:'在“净化”的基础上删除注释和不必要的单位表示，将 rgb 颜色转换为更短的十六进制表示'}"
+        mdui-tooltip="{content: '在“净化”的基础上删除注释和不必要的单位表示，将 rgb 颜色转换为更短的十六进制表示'}"
         @click="compress(1)"
         dense
         ripple
@@ -13,7 +13,7 @@
         压缩
       </mdui-btn>
       <mdui-btn
-        mdui-tooltip="{content:'在“压缩”的基础上合并属性与规则，删除重复的规则'}"
+        mdui-tooltip="{content: '在“压缩”的基础上合并属性与规则，删除重复的规则'}"
         @click="compress(2)"
         dense
         ripple
